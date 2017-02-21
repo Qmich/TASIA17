3dbcee48049beff34eff515f4114a2154f4382e8
Qmich <michaud.quentin@orange.fr>
2017-02-21 16:30:17 +0100

    harmonisation des " + ajout commentaire + sous programme decodage modifié

--- toto.py
+++ toto.py
@@ -7,9 +7,9 @@ import serial # bibliothèque permettant la communication série
 import time # pour le délai d'attente entre les messages
 import random # pour créer des chiffres aléatoires
 
-portUSB = serial.Serial('/dev/ttyACM0', 115200)
+portUSB = serial.Serial("/dev/ttyACM0", 115200)
 
-portCARBERRY = serial.Serial('/dev/ttyAMA0', 115200)
+portCARBERRY = serial.Serial("/dev/ttyAMA0", 115200)
 
 def envoi(variable, nomparametre):
 	message = nomparametre + ("={}".format(variable))
@@ -17,79 +17,75 @@ def envoi(variable, nomparametre):
 	time.sleep(0.01)
 
 def envoidetouteslesvariables():
-	envoi(braketempFL, 'braketempFL')
-	envoi(braketempFR, 'braketempFR')
-	envoi(braketempRL, 'braketempRL')
-	envoi(braketempRR, 'braketempRR')
-	envoi(gear, 'gear')
-	envoi(vehiclespeed, 'vehiclespeed')
-	envoi(TC, 'TC')
-	envoi(fuelconsumption, 'fuelconsumption')
-	envoi(enginetemp, 'enginetemp')
-	envoi(oiltemp, 'oiltemp')
-	envoi(mapposition, 'mapposition')
-	envoi(RPM, 'RPM')
-	envoi(boiteauto, 'boiteauto')
-	envoi(boitesequentielle, 'boitesequentielle')
+	envoi(braketempFL, "braketempFL")
+	envoi(braketempFR, "braketempFR")
+	envoi(braketempRL, "braketempRL")
+	envoi(braketempRR, "braketempRR")
+	envoi(gear, "gear")
+	envoi(vehiclespeed, "vehiclespeed")
+	envoi(TC, "TC")
+	envoi(fuelconsumption, "fuelconsumption")
+	envoi(enginetemp, "enginetemp")
+	envoi(oiltemp, "oiltemp")
+	envoi(mapposition, "mapposition")
+	envoi(RPM, "RPM")
+	envoi(boiteauto, "boiteauto")
+	envoi(boitesequentielle, "boitesequentielle")
 	
 def programmeCAN():
-	portCARBERRY.write('AT\r\n')
-	reponsetest = portCARBERRY.read()
-	if (reponsetest == 'OK\r\n')
-	 	braketempFL = receptiondonneeCAN('CH2', adresse, '100') 
-		braketempFR = receptiondonneeCAN('CH2', adresse, '100')
-		braketempRL = receptiondonneeCAN('CH2', adresse, '100')
-		braketempRR = receptiondonneeCAN('CH2', adresse, '100')
-		gear = receptiondonneeCAN('CH1', adresse, '200')
-		vehiclespeed = receptiondonneeCAN('CH1', adresse, '200')
+	portCARBERRY.write("AT\r\n")
+	envoipossible = testreceptionOK("initialisation Carberry")
+	
+	if (envoipossible == 1)
+	 	braketempFL = receptiondonneeCAN("CH2", adresse, "100") 
+		braketempFR = receptiondonneeCAN("CH2", adresse, "100")
+		braketempRL = receptiondonneeCAN("CH2", adresse, "100")
+		braketempRR = receptiondonneeCAN("CH2", adresse, "100")
+		gear = receptiondonneeCAN("CH1", adresse, "200")
+		vehiclespeed = receptiondonneeCAN("CH1", adresse, "200")
 		TC = receptiondonneeCAN()
-		fuelconsumption = receptiondonneeCAN('CH1', adresse, '200')
-		enginetemp = receptiondonneeCAN('CH1', adresse, '20')
-		oiltemp = receptiondonneeCAN('CH1', adresse, '20')
-		mapposition = receptiondonneeCAN('CH1', adresse, '10')
-		RPM = receptiondonneeCAN('CH1', adresse, '200')
+		fuelconsumption = receptiondonneeCAN("CH1", adresse, "200")
+		enginetemp = receptiondonneeCAN("CH1", adresse, "20")
+		oiltemp = receptiondonneeCAN("CH1", adresse, "20")
+		mapposition = receptiondonneeCAN("CH1", adresse, "10")
+		RPM = receptiondonneeCAN("CH1", adresse, "200")
 		boiteauto = receptiondonneeCAN()
 		boitesequentielle = receptiondonneeCAN()
-	else
-		message = ("erreur CARBERRY")
-		portUSB.write(message)
-		time.sleep(0.01)
+
 
 
 def receptiondonneeCAN(numeroCAN, adresse, rafraichissement)
 	mode = ("CAN MODE USER")
 	portCARBERRY.write(mode)
-	valeurerreur = testreceptionOK('mode')
+	etapesuivante = testreceptionOK("mode")
 	
-	if (valeurerreur = 1)
+	if (etapesuivante = 1)
 		open = ("CAN USER OPEN {} {}".format(numeroCAN, rafraichissement))
 		portCARBERRY.write(open)
-		valeurerreur = testreceptionOK('open')
+		etapesuivante = testreceptionOK("open")
 		
-		if (valeurerreur = 1)
+		if (etapesuivante = 1)
 			alignement = ("CAN USER ALIGN RIGHT")
 			portCARBERRY.write(alignement)
-			valeurerreur = testreceptionOK('alignement')
+			etapesuivante = testreceptionOK("alignement")
 			
-			if (valeurerreur = 1)
+			if (etapesuivante = 1)
 				ODBquerry = ("OBD QUERY {} {}".format(numeroCAN, adresse))
+				portCARBERRY.write(ODBquerry)
 				messagebrut = portCARBERRY.read()
-				valeur = decodage(messagebrut)
-	return valeur
+				valeurducapteur = decodage(messagebrut)
+	return valeurducapteur
 	
-def decodage(messagecarberrybrut)
-	i=0
-	while (messagecarberrybrut[i]!= '\r\n')
-		messagecarberry[i]=messagecarberrybrut[i]
-		i += 1
-	variablestring = messagecarberry.replace(' ', '')
-	variableint = int(variablestring)
+def decodage(messagecarberrybrut) # message brut du type : 1A 2B 32 <CR><LF>
+	messagecarberrybrut = messagecarberrybrut.replace(("\r","") # on enleve le <CR> du fin de message : 1A 2B 32 <LF>
+	messagecarberrybrut = messagecarberrybrut.replace(("\n","") # puis on enleve le <LF> : 1A 2B 32 
+	variablestring = messagecarberrybrut.replace(" ", "") # on enleve maintenant les espaces seperant les differents caracteres : 1A2B32 (du type str)
+	variableint = int(variablestring, 16) # on transforme le string en int tout en passant en base 10 : 1714994 (du type int)
 	return variableint
 
 def testreceptionOK(typeerreur):
 	reponsetest = portCARBERRY.read()
-	if (reponsetest == 'OK\r\n')
-		receptiondonneeCAN()
+	if (reponsetest == "OK\r\n")
 		return 1
 	else
 		message = ("erreur CARBERRY ") + typeerreur
@@ -100,6 +96,7 @@ def testreceptionOK(typeerreur):
 	
 
 while True: # boucle répétée jusqu'à l'interruption du programme
-	envoidetouteslesvariables()
+	
 	programmeCAN()
+	envoidetouteslesvariables()
 
